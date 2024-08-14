b8a8e7d8abfa0d47dd133632ac513f74eac333ce
xushengyun <254816773@qq.com>
2024-08-14 17:42:19 +0800

    修改代理配置

--- src/request/order.js
+++ src/request/order.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export function order(url) {
     return new Promise((resolve, reject) => {
         axios
-            // .get(url, {
-                .get('dataapi/' + url, {
+             .get(url, {
+               // .get('dataapi/' + url, {
                 headers: {
                     "Content-Type": "application/json",
                 },
