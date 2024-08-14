c40b860ab869be46c1db1778f9be06436a94f120
千喆 <1570978204@qq.com>
2024-08-14 08:47:59 +0000

    删除文件 browser

--- browser/src/components/bootstrap/CardImgOverlay.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div class="card-img-overlay">
-		<slot />
-	</div>
-</template>
--- browser/src/stores/pinia.ts
+++
@@ -1,6 +0,0 @@
-import { createPinia } from "pinia";
-// 创建
-const pinia = createPinia();
-
-// 导出
-export default pinia;
