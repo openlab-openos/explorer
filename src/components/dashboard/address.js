7599853fde0876e171fc6faccc5c7b8bd6336ccd
王晨阳 <1570978204@qq.com>
2025-02-07 10:15:05 +0800

    map

--- src/components/dashboard/address.js
+++ src/components/dashboard/address.js
@@ -180,8 +180,8 @@ export const ipAddresses = {
       code: "NA",
       timezone: "America/Chicago",
       country_name: "United States"
-    },  
-      {
+    },
+    {
       ip: "207.244.242.25",
       location: [38.6364, -90.1985],
       try: "US",
@@ -283,8 +283,8 @@ export const ipAddresses = {
     {
       ip: "154.26.159.163",
       location: [1.2805, 103.848],
-      try: "SG", 
-      code: "AS", 
+      try: "SG",
+      code: "AS",
       timezone: "Asia/Singapore",
       country_name: "Singapore"
     },
@@ -359,139 +359,188 @@ export const ipAddresses = {
       code: "AS",
       timezone: "Asia/Singapore",
       country_name: "Singapore"
-    },{
+    }, {
       ip: "46.250.255.174",
       location: [51.5094, -0.059],
       try: "GB",
       code: "EU",
       timezone: "Europe/London",
       country_name: "United Kingdom"
-    },{
+    }, {
       ip: "85.239.234.139",
       location: [47.3066, -122.2619],
       try: "US",
       code: "NA",
       timezone: "America/Los_Angeles",
       country_name: "United States"
-    },{
+    }, {
       ip: "85.239.234.30",
       location: [47.3066, -122.2619],
       try: "US",
-      code: "NA", 
+      code: "NA",
       timezone: "America/Los_Angeles",
       country_name: "United States"
-    },{
+    }, {
       ip: "85.239.234.139",
       location: [47.3066, -122.2619],
-      try: "US", 
-      code: "NA", 
+      try: "US",
+      code: "NA",
       timezone: "America/Los_Angeles",
       country_name: "United States"
-    },{
+    }, {
       ip: "46.250.255.174",
       location: [51.5094, -0.059],
       try: "GB", // 国家代码
       code: "EU", // 洲代码
       timezone: "Europe/London",
       country_name: "United Kingdom"
-    },{
+    }, {
       ip: "154.26.159.163",
       location: [1.2805, 103.848],
       try: "SG", // 国家代码
       code: "AS", // 洲代码
       timezone: "Asia/Singapore",
       country_name: "Singapore"
-    },{
+    }, {
       ip: "156.67.110.101",
       location: [19.0748, 72.8856],
       try: "IN", // 国家代码 (country_code)
       code: "AS", // 洲代码 (continent_code)
       timezone: "Asia/Kolkata",
       country_name: "India"
-    },{
+    }, {
       "ip": "154.53.38.170",
       "location": [38.5781, -90.4256],
       "try": "US",
       "code": "NA",
       "timezone": "America/Chicago",
       "country_name": "United States"
-    },{
+    }, {
       "ip": "5.104.87.77",
       "location": [35.0164, 136.7425],
       "try": "JP",
       "code": "AS",
       "timezone": "Asia/Tokyo",
       "country_name": "Japan"
-    },{
+    }, {
       "ip": "46.250.255.174",
       "location": [51.5094, -0.059],
       "country_code": "GB",
       "continent_code": "EU",
       "timezone": "Europe/London",
       "country_name": "United Kingdom"
-    },{
+    }, {
       "ip": "154.26.159.163",
       "location": [1.2805, 103.848],
       "country_code": "SG",
       "continent_code": "AS",
       "timezone": "Asia/Singapore",
       "country_name": "Singapore"
-    },{
+    }, {
       "ip": "154.53.38.170",
       "location": [38.5781, -90.4256],
       "country_code": "US",
       "continent_code": "NA",
       "timezone": "America/Chicago",
       "country_name": "United States"
-    },{
+    }, {
       "ip": "154.26.155.139",
       "location": [1.2805, 103.848],
       "country_code": "SG",
       "continent_code": "AS",
       "timezone": "Asia/Singapore",
       "country_name": "Singapore"
-    },{
+    }, {
       "ip": "193.22.146.84",
       "location": [48.9751, 8.4456],
-      "try": "DE", 
+      "try": "DE",
       "code": "AS51167",
       "timezone": "Europe/Berlin",
       "country_name": "Germany"
-    },{
+    }, {
       "ip": "194.233.82.186",
       "location": [1.3552, 103.8859],
-      "try": "SG", 
+      "try": "SG",
       "code": "AS141995",
       "timezone": "Asia/Singapore",
       "country_name": "Singapore"
-    },{
+    }, {
       ip: "144.126.155.214",
       location: [-90.1985, 38.6364], // 注意：经纬度顺序为 [latitude, longitude]
       try: "US", // 使用国家代码作为 try 字段
       code: "NA", // 使用洲代码作为 code 字段
       timezone: "America/Chicago",
       country_name: "United States"
-    },{
+    }, {
       ip: "38.242.223.80",
       location: [6.7897, 51.2216], // 经度在前，纬度在后
       try: "DE", // 使用国家代码作为 try 字段
       code: "EU", // 使用洲代码作为 code 字段
       timezone: "Europe/Berlin",
       country_name: "Germany"
-    },{
+    }, {
       ip: "84.247.161.146",
       location: [6.8607, 51.1878], // 经度在前，纬度在后
       try: "DE", // 使用国家代码作为 try 字段
       code: "EU", // 使用洲代码作为 code 字段
       timezone: "Europe/Berlin",
       country_name: "Germany"
-    },{
+    }, {
       ip: "89.117.144.192",
       location: [-90.1913, 38.6346], // 经度在前，纬度在后
       try: "US", // 使用国家代码作为 try 字段
       code: "NA", // 使用洲代码作为 code 字段
       timezone: "America/Chicago",
       country_name: "United States"
+    }, {
+      "ip": "89.117.144.192",
+      "location": [-90.1913, 38.6346], // 经度在前，纬度在后
+      "try": "US", // 使用国家代码作为 try 字段
+      "code": "NA", // 使用洲代码作为 code 字段
+      "timezone": "America/Chicago",
+      "country_name": "United States"
+    }, {
+      "ip": "193.22.146.84",
+      "location": [8.4456, 48.9751], // 经度在前，纬度在后
+      "try": "DE", // 使用国家代码作为 try 字段
+      "code": "EU", // 使用洲代码作为 code 字段
+      "timezone": "Europe/Berlin",
+      "country_name": "Germany"
+    }, {
+      "ip": "194.233.82.186",
+      "location": [103.8859, 1.3552], // 经度在前，纬度在后
+      "try": "SG", // 使用国家代码作为 try 字段
+      "code": "AS", // 使用洲代码作为 code 字段
+      "timezone": "Asia/Singapore",
+      "country_name": "Singapore"
+    }, {
+      "ip": "38.242.223.80",
+      "location": [6.7897, 51.2216], // 经度在前，纬度在后
+      "try": "DE", // 使用国家代码作为 try 字段
+      "code": "EU", // 使用洲代码作为 code 字段
+      "timezone": "Europe/Berlin",
+      "country_name": "Germany"
+    }, {
+      "ip": "85.239.232.234",
+      "location": [-122.2619, 47.3066], // 经度在前，纬度在后
+      "try": "US", // 使用国家代码作为 try 字段
+      "code": "NA", // 使用洲代码作为 code 字段
+      "timezone": "America/Los_Angeles",
+      "country_name": "United States"
+    }, {
+      "ip": "89.117.144.192",
+      "location": [-90.1913, 38.6346], // 经度在前，纬度在后
+      "try": "US", // 使用国家代码作为 try 字段
+      "code": "NA", // 使用洲代码作为 code 字段
+      "timezone": "America/Chicago",
+      "country_name": "United States"
+    }, {
+      "ip": "95.111.225.112",
+      "location": [8.1851, 48.9742], // 经度在前，纬度在后
+      "try": "FR", // 使用国家代码作为 try 字段
+      "code": "EU", // 使用洲代码作为 code 字段
+      "timezone": "Europe/Paris",
+      "country_name": "France"
     }
   ]
 };
