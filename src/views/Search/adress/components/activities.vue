3986c3e8df781908393274e1f8bf4171efb0568b
烮��惹�郸� <1570978204@qq.com>
2025-08-16 17:52:29 +0800

    edit

--- src/views/Search/adress/components/activities.vue
+++ src/views/Search/adress/components/activities.vue
@@ -1,11 +1,60 @@
 <template>
-    <div class="container"> 
-        1
-    </div>
+  <table
+    class="w-100 mb-0 small align-middle table table-striped table-borderless mb-2px small"
+  >
+    <tbody>
+      <tr>
+        <th>
+          {{ $t("transactions.signature") }}
+        </th>
+        <th>
+          {{ $t("navigation.blocks") }}
+        </th>
+        <th>
+          {{ $t("transactions.time") }}
+        </th>
+        <th>
+          {{ $t("transaction.fee_payer") }}
+        </th>
+        <th>
+          {{ $t("account.age") }}
+        </th>
+        <th>
+          BTG
+        </th>
+      </tr>
+      <template v-if="loading && type">
+        <tr v-for="(item, index) in paginatedHistoryData" :key="index">
+          <td class="text-theme">
+            
+          </td>
+          <td class="text-theme">
+
+          </td>
+          <td>
+
+          </td>
+          <td>
+
+          </td>
+          <td>
+
+          </td>
+          <td>
+
+          </td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+  <div v-if="historyData.length == 0 && loading" class="text-center">
+    {{ $t("account.available") }}
+  </div>
 </template>
 
 <script setup>
 import {
+  computed,
   onMounted,
   ref,
   watch,
@@ -18,13 +67,27 @@ const loading = ref(true);
 const props = defineProps({
   url: {
     type: String,
-    default: '',
+    default: "",
   },
   paramsId: {
     type: String,
-    default: '',
+    default: "",
   },
+});
+
+const historyData = ref([]);
+
+const currentPage = ref(1);
+const pageSize = ref(20);
+
+const paginatedHistoryData = computed(() => {
+  const start = (currentPage.value - 1) * pageSize.value;
+  const end = start + pageSize.value;
+
+  return historyData.value.slice(start, end);
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
